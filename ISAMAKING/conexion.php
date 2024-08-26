69f7ef8101535fec36bea9b149ae6f81da06af13
Alejo101220 <165951708+Alejo101220@users.noreply.github.com>
2024-08-26 11:26:51 -0500

    actualizacion proyecto

---
+++ ISAMAKING/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+$server ="localhost"; 
+$user = "root";
+$pass = "";
+$db ="isamaking";
+
+$conexion = new mysqli($server, $user, $pass, $db);
+
+if ($conexion->connect_errno) {
+    die("Conexion Fallida" . $conexion->connect_errno);
+} else{
+    echo "finalizado";
+}
+
+?>
